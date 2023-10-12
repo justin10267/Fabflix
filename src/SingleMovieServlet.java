f63b564d6a51db0a2eb9d88c6390de19197830c1
justin10267 <opiryg10267@gmail.com>
2023-10-11 23:11:03 -0700

    missing last step: hyperlinking movies on single star page

---
+++ src/SingleMovieServlet.java
@@ -0,0 +1,148 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+// Declaring a WebServlet called SingleMovieServlet, which maps to url "/api/single-movie"
+@WebServlet(name = "SingleMovieServlet", urlPatterns = "/api/single-movie")
+public class SingleMovieServlet extends HttpServlet {
+    private static final long serialVersionUID = 4L;
+
+    // Create a dataSource which registered in web.xml
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Retrieve parameter id from url request.
+        String id = request.getParameter("id");
+
+        // The log message can be found in localhost log
+        request.getServletContext().log("getting id: " + id);
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try (Connection conn = dataSource.getConnection()) {
+            // Get a connection from dataSource
+
+            // Construct a query with parameter represented by "?"
+            String query = "SELECT \n" +
+                    "    m.id,\n" +
+                    "    m.title,\n" +
+                    "    m.year,\n" +
+                    "    m.director,\n" +
+                    "    (SELECT \n" +
+                    "            GROUP_CONCAT(g.name\n" +
+                    "                    ORDER BY g.name DESC)\n" +
+                    "        FROM\n" +
+                    "            genres_in_movies gm\n" +
+                    "                INNER JOIN\n" +
+                    "            genres g ON gm.genreId = g.id\n" +
+                    "        WHERE\n" +
+                    "            gm.movieId = m.id) AS genres,\n" +
+                    "    (SELECT \n" +
+                    "            GROUP_CONCAT(CONCAT(s.id, ':', s.name)\n" +
+                    "                    ORDER BY s.name DESC , s.id)\n" +
+                    "        FROM\n" +
+                    "            stars_in_movies sm\n" +
+                    "                INNER JOIN\n" +
+                    "            stars s ON sm.starId = s.id\n" +
+                    "        WHERE\n" +
+                    "            sm.movieId = m.id) AS stars,\n" +
+                    "    r.rating\n" +
+                    "FROM\n" +
+                    "    movies m\n" +
+                    "        JOIN\n" +
+                    "    ratings r ON m.id = r.movieId\n" +
+                    "WHERE\n" +
+                    "    m.id = ?\n" +
+                    "GROUP BY m.id , m.title , m.year , m.director , r.rating;";
+
+            // Declare our statement
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            // Set the parameter represented by "?" in the query to the id we get from url,
+            // num 1 indicates the first "?" in the query
+            statement.setString(1, id);
+
+            // Perform the query
+            ResultSet rs = statement.executeQuery();
+
+            JsonArray jsonArray = new JsonArray();
+
+            // Iterate through each row of rs
+            while (rs.next()) {
+                String movie_id = rs.getString("id");
+                String movie_title = rs.getString("title");
+                String movie_year = rs.getString("year");
+                String movie_director = rs.getString("director");
+                String movie_genres = rs.getString("genres");
+                String movie_stars = rs.getString("stars");
+                String movie_rating = rs.getString("rating");
+
+                // Create a JsonObject based on the data we retrieve from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("movie_id", movie_id);
+                jsonObject.addProperty("movie_title", movie_title);
+                jsonObject.addProperty("movie_year", movie_year);
+                jsonObject.addProperty("movie_director", movie_director);
+                jsonObject.addProperty("movie_genres", movie_genres);
+                jsonObject.addProperty("movie_stars", movie_stars);
+                jsonObject.addProperty("movie_rating", movie_rating);
+
+                jsonArray.add(jsonObject);
+            }
+            rs.close();
+            statement.close();
+
+            // Write JSON string to output
+            out.write(jsonArray.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Log error to localhost log
+            request.getServletContext().log("Error:", e);
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+
+        // Always remember to close db connection after usage. Here it's done by try-with-resources
+
+    }
+
+}
