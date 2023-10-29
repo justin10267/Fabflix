0d6bf77700de854fa4ec9b6b5a7680fb4f7b7dbb
JixingGithub <bianjixing@gmail.com>
2023-10-28 21:13:28 -0700

    Front end modify, project 1

--- WebContent/cart.js
+++ WebContent/cart.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 window.onload = function() {
     fetchCartData();
 };
@@ -23,3 +24,30 @@ function fetchCartData() {
             console.error('Error fetching cart data:', error);
         });
 }
+=======
+window.onload = function() {
+    fetchCartData();
+};
+
+function fetchCartData() {
+    fetch('/api/cart', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            let cartDiv = document.getElementById('cartContent');
+            cartDiv.innerHTML = ''; // Clear current content
+
+            // Loop through each item in the cart and display it
+            data.forEach(item => {
+                cartDiv.innerHTML += `<p>${item.movie_title} - ${item.quantity}</p>`;
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching cart data:', error);
+        });
+}
+>>>>>>> 83fc3c7 (Front end modify, project 1)
--- src/ShoppingCartServlet.java
+++ src/ShoppingCartServlet.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import jakarta.servlet.ServletConfig;
@@ -96,3 +97,103 @@ public class ShoppingCartServlet extends HttpServlet {
 }
 
 
+=======
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@WebServlet(name = "ShoppingCartServlet", urlPatterns = "/api/cart")
+public class ShoppingCartServlet extends HttpServlet {
+    private static final long serialVersionUID = 4L;
+
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        HttpSession session = request.getSession();
+        ArrayList<String> cart = (ArrayList<String>) session.getAttribute("cart");
+
+        if (cart == null) {
+            cart = new ArrayList<>();
+        }
+
+        try (Connection conn = dataSource.getConnection()) {
+            Statement statement = conn.createStatement();
+
+            JsonArray jsonArray = new JsonArray();
+
+            // For a more realistic scenario, cart should be an ArrayList of Movie or MovieId objects,
+            // and you'd fetch movie details for each id in the cart.
+            for (String movieId : cart) {
+                String query = "SELECT ...";  // query to fetch details for movieId
+
+                ResultSet rs = statement.executeQuery(query);
+
+                if (rs.next()) {
+                    // Convert result set into JsonObject and add to jsonArray
+                }
+
+                rs.close();
+            }
+            statement.close();
+
+            out.write(jsonArray.toString());
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String action = request.getParameter("action");  // e.g., "add", "remove", etc.
+        String movieId = request.getParameter("movieId");
+
+        HttpSession session = request.getSession();
+        ArrayList<String> cart = (ArrayList<String>) session.getAttribute("cart");
+
+        if (cart == null) {
+            cart = new ArrayList<>();
+        }
+
+        if ("add".equals(action)) {
+            cart.add(movieId);
+        } else if ("remove".equals(action)) {
+            cart.remove(movieId);
+        }
+
+        session.setAttribute("cart", cart);
+        response.setStatus(200);
+    }
+}
+
+
+>>>>>>> df8357c (Front end modify, project 1)
