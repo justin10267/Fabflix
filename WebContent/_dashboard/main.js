cf39140ea2e4c915a98f73c0777787056719b3c1
JixingGithub <bianjixing@gmail.com>
2023-11-12 01:38:23 -0800

    Front end modify, project 1

--- WebContent/_dashboard/main.js
+++ WebContent/_dashboard/main.js
@@ -1,29 +1,46 @@
 function handleResult(resultData) {
     console.log(resultData);
     const tablesContainer = document.getElementById('tablesContainer');
-    resultData.forEach(table => {
-        const tableElem = document.createElement('table');
-        const caption = tableElem.createCaption();
-        caption.textContent = table.tableName;
 
-        const headerRow = tableElem.insertRow();
+    resultData.forEach(table => {
+        // Create a container for each table and its name
+        const tableContainer = document.createElement('div');
+        tableContainer.className = 'table-container';
+
+        // Create and append the table name element
+        const tableNameElem = document.createElement('h2');
+        tableNameElem.className = 'table-name';
+        tableNameElem.textContent = table.tableName;
+        tableContainer.appendChild(tableNameElem);
+
+        const tableElem = document.createElement('table');
+        tableElem.className = 'table';
+
+        const thead = tableElem.createTHead(); // Create thead element
+        const headerRow = thead.insertRow();
+
         ['Attribute', 'Type'].forEach(headerText => {
             const th = document.createElement('th');
             th.textContent = headerText;
             headerRow.appendChild(th);
         });
 
+        const tbody = document.createElement('tbody'); // Create tbody element
+        tableElem.appendChild(tbody); // Append tbody to the table
+
         table.columns.forEach(column => {
-            const row = tableElem.insertRow();
+            const row = tbody.insertRow(); // Insert row into tbody instead of table
             const columnName = row.insertCell(0);
             const columnType = row.insertCell(1);
             columnName.textContent = column.columnName;
             columnType.textContent = column.columnType;
         });
 
-        tablesContainer.appendChild(tableElem);
+        tableContainer.appendChild(tableElem);
+        tablesContainer.appendChild(tableContainer);
     });
 }
+
 
 jQuery.ajax({
     dataType: "json",
